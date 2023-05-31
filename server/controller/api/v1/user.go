d042107267987edd84bccf65e5a46ded3d8c8500
wzrtzt <wangzhao0216@foxmail.com>
2023-05-31 17:09:25 +0800

    Cookies

--- server/controller/api/v1/user.go
+++ server/controller/api/v1/user.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"blog_server/utils"
 	"fmt"
 	"net/http"
 
@@ -20,6 +21,7 @@ func (u *User) Add(ctx *gin.Context) {
 
 func (u *User) Login(ctx *gin.Context) {
 	fmt.Println(ctx.Request.Header)
+	ctx.SetCookie("Token", "token_token_token", 24*60*60, "/", utils.Domain, true, false)
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg":       "login",
 		"host_name": ctx.Request.Host,
